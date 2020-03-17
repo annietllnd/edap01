9fe89c08cba05a992026d43623a7218d48ab43ab
annietallund <annietallund@gmail.com>
2020-03-17 17:44:13 +0100

    update model for assignment completion, omit GUI for now

--- reversi/model.py
+++ reversi/model.py
@@ -1,3 +1,6 @@
+import sys
+import copy
+
 
 class Game:
 
@@ -7,12 +10,17 @@ class Game:
 		self.turn = -1
 		self.human = self.turn
 		self.current_board = self.make_board()
+		self.search = Search(self)
 
-	def test_board(self):
-		list = []
-		list.append(Tile(-1, [], self.current_board))
-		print(list[0].state)
-		print("yay")
+	def print_board(self, board):
+		for row in range(0, self.rows):
+			print("\n")
+			for col in range(0, self.cols):
+				print(board[row][col].get_state(), end='    ')
+
+		print("\n")
+		print("computer score:", self.get_score(self.computer), "player score:", self.get_score(self.player))        
+		print("\n")
 
 
 	def make_board(self):
@@ -27,21 +35,119 @@ class Game:
 	def get_tile(self, row: int, col: int):
 		return self.current_board[row][col]
 
-	def choose_player(self, selection):
-		if selection == 1:
-			self.human = "black"
-		else:
-			self.human = "white"
-		self.turn = self.human
+	def get_board_copy(self, board):
+		return copy.deepcopy(board)
 
-	def make_move(self, row: int, col: int, turn: str):
-		if(self.is_valid_move(row, col, turn)):
-			self.get_board()[row][col] = turn
+	def make_move(self, board, row: int, col: int, turn: int):
+		_, tilesToFlip = self.valid_moves(board, row, col, turn)
+		if tilesToFlip == []:
+			return False
 
-	def is_valid_move(self, row: int, col: int, turn: str) -> bool:
-		return (self.rows > row) and (self.cols > col) and (self.get_cell_color(row, col) != turn) or (self.get_cell_color(row, col) == None)
+		tilesToFlip = tilesToFlip[0]
+		board[row][col].set_state(turn)
+		for x, y in tilesToFlip:
+			board[x][y].set_state(turn)
+		return True
 
-	def get_cell_color(self, row: int, col: int) -> int:
+	def get_valid_moves_board(self, turn: int):  # Returns a new board with 'X' marking the valid moves for the player.
+		boardCopy = self.get_board_copy(self.get_board())
+		valid_moves, _ = self.get_valid_moves(boardCopy, turn)
+		for row, col in valid_moves:
+			boardCopy[row][col].set_state(2) 
+		return boardCopy
+
+
+	def get_valid_moves(self, board, turn: int):
+		moves = []
+		tiles_per_move = []
+		for row in range(0, self.rows):
+			for col in range(0, self.rows):
+				(valid_moves, tiles_per_move) = self.valid_moves(board, row, col, turn)
+				if (valid_moves != []):
+					moves.append((row, col))
+
+		return moves, tiles_per_move
+
+	# returns a list of the valid moves and the tiles that are to be flipped
+	# based on tile at (row, col) for turn on board, 
+	# and two empty lists if none.
+	def valid_moves(self, board, row: int, col: int, turn: int):
+		neighbours = []
+		valid_neighbours = []
+		coordinates = []
+		tiles_per_move = []
+		tile = board[row][col]
+		if (self.is_valid_cell(row, col) and self.is_empty_cell(row, col)):
+			neighbours = tile.get_neighbours()
+
+		for n in neighbours:
+			n_row, n_col = n.get_coordinates()
+			n_row = n_row - row
+			n_col = n_col - col
+
+			valid, tilesToFlip = self.is_valid_directional_move(row, col, n_row, n_col, turn)
+
+			if(valid and n.get_state() != tile.get_state()):
+				valid_neighbours.append(n)
+				tiles_per_move.append(tilesToFlip)
+			
+		for t in valid_neighbours:
+			state = t.get_state()
+	
+			if(state != turn):
+				move_row, move_col = t.get_coordinates() 
+				if not (move_row == row and move_col == col):
+					coordinates.append((row, col))
+		return coordinates, tiles_per_move
+
+
+	def is_empty_cell(self, row, col):
+		return self.get_cell_state(row, col) == 0
+
+	def is_valid_cell(self, row: int, col: int) -> bool:
+		return (self.rows > row) and (self.cols > col)
+
+
+	def flip_cell(self, row: int, col: int, turn: int):
+		self.get_board()[row][col].flip()   
+		self.print_board(self.get_board())
+	
+
+	def is_valid_directional_move(self, row: int, col: int, n_row: int, n_col: int, turn: int):
+		current_row = n_row + row 
+		current_col = n_col + col
+		tilesToFlip = []
+
+		stop_cell_color = self.get_opposite_turn(turn)
+
+		while True:
+
+			if not (self.is_valid_cell(current_row, current_col)):
+				break
+			if (self.get_cell_state(current_row, current_col) == 0):
+				break
+			if (self.get_cell_state(current_row, current_col) == turn):
+
+				rowdelta = n_row+row
+				coldelta = n_col+col
+				state = self.get_cell_state(rowdelta, coldelta)
+
+				while(state == stop_cell_color and state != 0):
+					tilesToFlip.append((rowdelta, coldelta))
+					rowdelta += n_row
+					coldelta += n_col
+					state = self.get_cell_state(rowdelta, coldelta)
+				
+				stop_cell_color = turn
+				break
+
+			current_row += n_row
+			current_col += n_col
+			
+		return stop_cell_color == turn, tilesToFlip
+
+
+	def get_cell_state(self, row: int, col: int) -> int:
 		return self.get_board()[row][col].get_state()
 
 	def get_board(self):
@@ -60,10 +166,63 @@ class Game:
 		self.turn = self.get_opposite_turn(self.turn)
 
 	def get_opposite_turn(self, current_turn: int) -> int:
-		return {-1:1, 1:-1}[current_turn]
+		return current_turn*-1
+
+	def get_corners(self, turn: int) -> int:
+		count = 0
+		for i in [0, 7]:
+			for j in [0, 7]:
+				if(self.get_board()[i][j].get_state() == turn):
+					count += 1
+		return count
 
 	def get_score(self, turn: int) -> int:
-		return sum(row.count(turn) for row in self.get_board())
+		board = self.get_board()
+		return sum(abs(self.get_board()[i][j].get_state()) for i in range(0,self.rows) for j in range(0,self.cols) if self.get_board()[i][j].get_state() == turn)
+
+	def enter_player_tile(self):    # The first element in the list is the player's tile, the second is the computer's tile.
+		tile = '0'
+		while not (tile == '-1' or tile == '1'):
+			print("Do you want to be -1 or 1? (1 goes first)")
+			tile = input()
+		if tile == '1':
+			return [1, -1]
+		else:
+			return [-1, 1]
+
+	def who_goes_first(self, player):
+		if player == -1:
+			self.computer = 1
+			self.player = -1
+			return 'computer'
+		else:
+			self.computer = -1
+			self.player = 1
+			return 'player'
+
+	def get_player_move(self, player):
+		DIGITS = '1 2 3 4 5 6 7 8'.split()
+		while True:
+			print("Enter your move as coordinates 'xy', or type 'quit' to end the game. 'X' show possible legal moves.")
+			move = input().lower()
+			if move == 'quit':
+				return 'quit'
+			if len(move) == 2 and move[0] in DIGITS and move[1] in DIGITS:
+				x = int(move[0]) - 1
+				y = int(move[1]) - 1
+
+				if (self.is_valid_directional_move(x, y, 0, 0, player) == True):
+					continue
+				else:
+					break
+			else:
+				print("Not a valid move.")
+		return [x, y]
+
+	def get_computer_move(self):
+		_, best_move = self.search.minmax(self.get_board(), 4, 10000, -10000, self.computer, self.computer)
+		print("Best move: ", best_move)
+		return best_move
 
 
 class Tile:
@@ -74,32 +233,134 @@ class Tile:
 		self.col = col
 		self.game = game
 
+	
+	def get_coordinates(self):
+		return self.row, self.col
+	
+	def flip(self):
+		if(self.state < 0):
+			self.state = 1
+		else:
+			self.state = -1
+
+	def set_state(self, state: int):
+		self.state = state
+
 	def get_state(self):
 		return self.state
 
-	def get_color():
-		if self.state == -1:
-			return "white"
-		elif self.state == 1:
-			return "black"
-		else:
-			return "green"
-
 	def get_neighbours(self):
-		if(self.row > 0 and self.col > 0):
-			neighbours = [self.game.get_board()[self.row+i][self.col+j] for i in range(-1,2) for j in range(-1,2) if not ((i == 0) and (j == 0))]
-		elif(self.row > 0):
-			neighbours = [self.game.get_board()[self.row+i][self.col+j] for i in range(-1,2) for j in range(0,2) if not ((i == 0) and (j == 0))]
-		elif(self.col > 0):
-			neighbours = [self.game.get_board()[self.row+i][self.col+j] for i in range(0,2) for j in range(-1,2) if not ((i == 0) and (j == 0))]
-
+		neighbours = []
+		neighbours = [self.game.get_board()[self.row+i][self.col+j] for i in range(-1,2) for j in range(-1,2) if (not ((i == 0) and (j == 0)) and self.game.is_valid_cell(self.row+i, self.col+j))]
+		
 		return neighbours
+
+class Search: 
+
+	def __init__(self, game: Game):
+		self.game = game
+
+	def heuristic(self, turn: int, moves):
+		tile_count = self.game.get_score(turn)
+		corners = self.game.get_corners(turn)
+
+		return 0.01*tile_count + len(moves) + 10*corners
+
+
+	def minmax(self, board, depth: int, alpha: int, beta: int, maximizingPlayer: int, turn: int):
+		threshold = 10000
+		moves, _ = self.game.get_valid_moves(board, maximizingPlayer)
+		best_move = (-1,-1)
+
+
+		if(depth == 0 or moves == []):
+			heuristic = self.heuristic(turn, moves)
+			return heuristic, best_move
+
+		if(maximizingPlayer == turn):
+			maxEval = -threshold
+			for move in moves:
+				eval_board = self.game.get_board_copy(board)
+				self.game.make_move(eval_board, move[0], move[1], turn)
+				res, _ = self.minmax(eval_board, depth-1, alpha, beta, maximizingPlayer, turn)
+				maxEval = max(maxEval, res)
+				if(maxEval == res):
+					best_move = move 	
+				alpha = max(alpha, res)
+				if(beta <= alpha):
+					break
+			return maxEval, best_move
+		else:
+			minEval = threshold
+			for move in moves:
+				eval_board = self.game.get_board_copy(board)
+				self.game.make_move(eval_board, move[0], move[1], self.game.get_opposite_turn(turn))
+				res = self.minmax(eval_board, depth-1, alpha, beta, maximizingPlayer, turn)
+				minEval = min(minEval, res)
+				if(minEval == res):
+					best_move = move
+				beta = min(beta, res)
+				if(beta <= alpha):
+					break
+			return minEval, best_move
 
 def main():
 	game = Game(8,8)
 	board = game.get_board()
-	#print(board)
-	print(board[1][0].get_neighbours())
+	board[3][3].set_state(-1)
+	board[3][4].set_state(1)
+	board[4][3].set_state(1)
+	board[4][4].set_state(-1)
+
+	while True:
+		player, computer = game.enter_player_tile()
+		turn = game.who_goes_first(player)
+		print(turn, "will go first")
+
+		while True:			
+			if turn == 'player':
+				#validMovesBoard = game.get_valid_moves(game.get_board(), player)
+				game.print_board(game.get_board())
+				move = game.get_player_move(player)
+
+				if move == 'quit':
+					print("Thanks for playing!")
+					sys.exit()
+				else:
+					game.make_move(game.get_board(), move[0], move[1], player)
+					game.print_board(game.get_board())
+				if game.get_valid_moves(game.get_board(), computer)[0] == []:
+					break
+				else:
+					turn = 'computer'
+			else:
+				input("Press Enter to see the computer\'s move.")
+				move = game.get_computer_move()
+				game.make_move(game.get_board(), move[0], move[1], computer)
+				if game.get_valid_moves(game.get_board(), player)[0] == []:
+					break
+				else:
+					turn = 'player'
+
+
+		game.print_board(mainBoard)
+		if(computer == -1):
+			score_computer = game.get_score(-1)
+			score_player = game.get_score(1)
+		else:
+			score_computer = game.get_score(1)
+			score_player = game.get_score(-1)
+		print(f"computer scored {score_computer} points. player scored {score_player} points.")
+		if score_player > score_computer:
+			print("You beat the computer by %s points! Congratulations!" % (scores_player - scores_computer))
+		elif scores[playerTile] < scores[computerTile]:
+			print("You lost. The computer beat you by %s points." % (scores_computer - scores_player))
+		else:
+			print("The game was a tie!")
+
+		if not play_again():
+			print("Thanks for playing!")
+			break
 
 if __name__ == "__main__":
 	main()
--- reversi/reversi.py
+++ reversi/reversi.py
@@ -10,7 +10,7 @@ class Reversi:
 		self._board = Canvas(master = root_window, height=height, width=width)
 		self._width = width
 		self._height = height
-		self._rectangles = [None for i in range(NBR_COLS*NBR_ROWS)]
+		self._rectangles = [None for i in range(NBR_COLS)]
 		self._rows = self._game.get_rows()
 		self._cols = self._game.get_cols()
 		self._game_board = self._game.get_board()
@@ -18,21 +18,37 @@ class Reversi:
 	def get_board(self) -> Canvas:
 		return self._board
 
+	def reload_board(self):
+		for i in range(0, NBR_ROWS):
+			for j in range(0, NBR_COLS):
+				self._board.itemconfig(self._rectangles[i][j], fill=self.get_color(self._game_board[i][j]))
+
+		print(self._game_board)
+
 	def makeBoard(self):
 		dist = self._width/(self._rows)
 		for i in range(self._rows):
+			self._rectangles[i] = []
 			for j in range(self._cols):
-				self._rectangles[i*NBR_ROWS+j] = self._board.create_rectangle(j*dist, i*dist, (j+1)*dist, (i+1)*dist, fill=self.get_color(self._game_board[i][j].get_state()))
-				self._board.tag_bind(self._rectangles[i*NBR_ROWS+j],"<Button-1>",self.onClick)
+				self._rectangles[i].append(self._board.create_rectangle(j*dist, i*dist, (j+1)*dist, (i+1)*dist, fill=self.get_color(self._game_board[i][j].get_state())))
+				self._board.tag_bind(self._rectangles[i][j],"<Button-1>",self.onClick)
 		self._board.pack()
 
-		self._board.itemconfig(self._rectangles[27], fill=self.get_color(self._game.get_turn()))
-		self._board.itemconfig(self._rectangles[28], fill=self.get_color(self._game.get_opposite_turn(self._game.get_turn())))
-		self._board.itemconfig(self._rectangles[35], fill=self.get_color(self._game.get_opposite_turn(self._game.get_turn())))
-		self._board.itemconfig(self._rectangles[36], fill=self.get_color(self._game.get_turn()))
+		self._board.itemconfig(self._rectangles[1][2], fill=self.get_color(self._game.get_turn()))
+		self._board.itemconfig(self._rectangles[2][1], fill=self.get_color(self._game.get_opposite_turn(self._game.get_turn())))
+		self._board.itemconfig(self._rectangles[3][5], fill=self.get_color(self._game.get_opposite_turn(self._game.get_turn())))
+		self._board.itemconfig(self._rectangles[3][6], fill=self.get_color(self._game.get_turn()))
 
 	def onClick(self, evt=None):
-		self._board.itemconfig(self._board.find_withtag(CURRENT), fill=self._game.get_turn())
+		self._board.itemconfig(self._board.find_withtag(CURRENT), fill=self.get_color(self._game.get_turn()))
+		current = self._board.find_withtag(CURRENT)[0]
+		tuple = [(index, row.index(current)) for index, row in enumerate(self._rectangles) if current in row]
+		row = tuple[0][0]
+		col = tuple[0][1]
+		self._game_board[row][col].set_state(self._game.get_turn())
+		self.fill(1, 5, 3, 7, -1)
+		print("hehe")
+
 
 	def get_color(self, state: int):
 		if state == -1:
@@ -42,8 +58,31 @@ class Reversi:
 		elif state == 0:
 			return "green3"
 
-		#Ladda in lista
-		#Dialogruta där man får välja färg
-		#Assigna färgen till spelare
-		#Rita upp startpositionen (förslagsvis i listan)
-		#
+	def flip_cell(self, row: int, col: int):
+		self._game_board[row][col] = self._game.get_opposite_turn(self._game.get_turn()) 
+		print("cell flipped to ", self._game.get_turn())
+
+	def fill(self, start_row: int, start_col: int, end_row: int, end_col: int, turn: int):
+		
+		#if(start_row <= end_row and start_col <= end_col):
+			for i in range(start_row+1, end_row):
+			  for j in range(start_col+1, end_col):
+			  	#self._board.itemconfig(self._rectangles[i][j], fill=self.get_color(self._game.get_turn()))
+			  	self._game_board[i][j].set_state(self._game.get_turn())
+			  	self.reload_board()
+			  	print("hoho")
+	#	elif(start_col < end_row):
+	#		for i in range(start_row+1, end_row):
+#			  for j in range(start_col-1, end_col, -1):
+#			  	#self._board.itemconfig(self._rectangles[i][j], fill=self.get_color(self._game.get_turn()))
+#			  	self._game_board[i][j].set_state(self._game.get_turn())
+#			  	self.reload_board()
+#	#	else:
+#			for i in range(start_row-1, end_row, -1):
+#			  for j in range(start_col+1, end_col):
+#			  	print("hoho")
+#			  	#self._board.itemconfig(self._rectangles[i][j], fill=self.get_color(self._game.get_turn()))
+#			  	self._game_board[i][j].set_state(self._game.get_turn())
+#			  	self.reload_board()
+
+		     
